9a4de4cd9e9edd9d1c9b818e009f60efff2ed9fb
BernardoSousa03 <bernardo.de.sousa@tecnico.ulisboa.pt>
2022-10-23 20:19:59 +0100

    order clients by alfabetic order (not differentiating between upper case and lower case)

--- prr/core/ClientComparator.java
+++ prr/core/ClientComparator.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class ClientComparator implements Comparator<Client> {
     @Override
     public int compare(Client c1, Client c2){
-        return c1.getKey().compareTo(c2.getKey());
+        return c1.getKey().toLowerCase().compareTo(c2.getKey().toLowerCase());
     }
 }
