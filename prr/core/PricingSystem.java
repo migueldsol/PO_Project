9735ad7bfd4c7beae0ec60d948b5a734017a7fdf
BernardoSousa03 <bernardo.de.sousa@tecnico.ulisboa.pt>
2022-11-04 14:44:09 +0000

    self terminal text works and added a nem to pricing system

--- prr/core/PricingSystem.java
+++ prr/core/PricingSystem.java
@@ -1,18 +1,27 @@
 package prr.core;
 
 
-public interface PricingSystem{
+public abstract class PricingSystem{
+
+    private final String PRICING_SYSTEM_NAME;
+
+    public PricingSystem(String name){
+        PRICING_SYSTEM_NAME = new String(name);
+    }
     
-    int getNormalTextTariff(int length);
-    int getGoldTextTariff(int length);
-    int getPlatinumTextTariff(int length);
+    public abstract int getNormalTextTariff(int length);
+    public abstract int getGoldTextTariff(int length);
+    public abstract int getPlatinumTextTariff(int length);
 
-    int getNormalVoiceTariff(int duration);
-    int getGoldVoiceTariff(int duration);
-    int getPlatinumVoiceTariff(int duration);
+    public abstract int getNormalVoiceTariff(int duration);
+    public abstract int getGoldVoiceTariff(int duration);
+    public abstract int getPlatinumVoiceTariff(int duration);
 
-    int getNormalVideoTariff(int duration);
-    int getGoldVideoTariff(int duration);
-    int getPlatinumVideoTariff(int duration);
+    public abstract int getNormalVideoTariff(int duration);
+    public abstract int getGoldVideoTariff(int duration);
+    public abstract int getPlatinumVideoTariff(int duration);
 
+    public String getName(){
+        return PRICING_SYSTEM_NAME;
+    }
 }
